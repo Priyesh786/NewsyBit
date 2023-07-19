6d438baaa9945edd8d1ae8e8cc33b9913b269886
PRIYESH ANAND <priyeshanand9@gmail.com>
2023-07-19 16:51:57 +0530

    Initial commit

--- backend/dbConnect.js
+++ backend/dbConnect.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 
-mongoose.connect('mongodb+srv://priyeshanand9:TRr2MgufCfooKVil@cluster0.kwhssjk.mongodb.net/user' , {useUnifiedTopology:true, useNewUrlParser:true})
+const connectionString = process.env.MONGODB_URI;
 
-const connection = mongoose.connection
-connection.on('connected', ()=> {
-    console.log("Connection successful")
-})
-connection.on('error', ()=> {
-    console.log("Connection failed")
-})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+  });
+  
+  mongoose.connection.on('error', (error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+  
+  mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+      console.log('Connection successful');
+    })
+    .catch((error) => {
+      console.error('Connection failed:', error);
+    });
+  
+  module.exports = mongoose;
 
 module.exports = mongoose
